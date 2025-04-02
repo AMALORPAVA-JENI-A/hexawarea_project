a0fd2d2df32614471a4cc161451918d3ec27faaf
AMALORPAVA-JENI-A <amalorpavajeni.a@gmail.com>
2025-04-03 00:33:12 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsAssignment
+{
+    internal class Account
+    {
+        int AccountNumber;
+        string AccountType;
+        float AccountBalance;
+        public int accountNumber
+        {
+            set { AccountNumber = value; }
+            get { return AccountNumber; }
+        }
+        public string accountType
+        {
+            get { return AccountType; }
+            set { AccountType = value; }
+        }
+        public float accountBalance
+        {
+            get { return AccountBalance; }
+            set { AccountBalance = value; }
+        }
+        public Account()
+        {
+
+        }
+        public void Deposit(float amount)
+        {
+            if (amount > 0)
+            {
+                AccountBalance += amount;
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount");
+            }
+        }
+        public void WithDraw(float amount)
+        {
+            if (amount < AccountBalance)
+            {
+                AccountBalance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+        }
+        public void CalculateInterest(float rate)
+        {
+            Console.WriteLine($"The interest is {AccountBalance * rate * 1}");
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Account Number:{AccountNumber} Account Type:{AccountType} Account Balance:{AccountBalance}");
+        }
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsAssignment
+{
+    internal class Customer
+    {
+        int CustomerID;
+        string FirstName;
+        string LastName;
+        string EmailAdress;
+        string PhoneNo;
+        string Address;
+
+        public int customerID
+        {
+            set{CustomerID=value;}
+            get{return CustomerID;}
+        }
+        public string firstName
+        {
+            set{FirstName=value;}
+            get { return FirstName; }
+        }
+            public string lastName
+        {
+            set{LastName=value;}
+            get { return LastName; }
+        }
+        public string emailAdress
+        {
+            set{EmailAdress=value;}
+            get { return EmailAdress;}
+        }
+        public string phoneNo
+        {
+            set { PhoneNo=value;}
+            get { return PhoneNo; }
+        }
+        public string address
+        {
+            set { Address=value;}
+            get { return Address; }
+        }
+        public Customer()
+        {
+        }
+        public void Display(int CustomerID,string FirstName,string Lastname,string EmailAddress,string PhoneNo,string Address)
+        {
+            Console.WriteLine($"Customer ID:{CustomerID} First Name:{FirstName} Last Name:{LastName} Email Address:{EmailAddress}  Phone Number:{PhoneNo} Address:{Address}");
+        }
+
+    }
+}
