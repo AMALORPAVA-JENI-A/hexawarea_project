e7332da8ae584fdf485fd44724a077ef6be27ffc
AMALORPAVA-JENI-A <amalorpavajeni.a@gmail.com>
2025-04-15 14:05:48 +0530

    Add files via upload

---
+++ HMBank/Customer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace HMBank
+{
+    public class Customer
+    {
+
+
+        public int counter 
+            { get; set; }
+
+            public int customerID
+            {
+                set;
+                get;
+            }
+            public string firstName
+            {
+                set;
+                get;
+            }
+            public string lastName
+            {
+                set;
+                get;
+            }
+            public string emailAdress
+            {
+                set;
+                get;
+            }
+            public string phoneNo
+            {
+                set;
+                get;
+            }
+            public string address
+            {
+                set;
+                get;
+            }
+            public Customer()
+            {
+            }
+            public Customer(int customerID,string FirstName, string LastName, string EmailAddress, string PhoneNo, string Address)
+            {
+                if (!Regex.IsMatch(EmailAddress, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+                {
+                    Console.WriteLine("Invalid email");
+                }
+                if (!Regex.IsMatch(PhoneNo, @"^\d{10}$"))
+                {
+                    Console.WriteLine("Invalid Phone number");
+                }
+                this.customerID = customerID;
+                firstName = FirstName;
+                lastName = LastName;
+                emailAdress = EmailAddress;
+                phoneNo = PhoneNo;
+                address = Address;
+
+
+            }
+            public override string ToString()
+            {
+                return $"Customer ID:{customerID} First Name:{firstName} Last Name:{lastName} Email Address:{emailAdress}  Phone Number:{phoneNo} Address:{address}";
+            }
+
+
+        }
+    }
+
+
